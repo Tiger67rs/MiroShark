9a6a7c9b7fe95a12de9f8442dc2e8e81f24ac9db
Tiger67rs <tiger67rs@Yahoo.com>
2026-05-16 16:47:04 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    allowedHost: ['miroshark-production-86de.up.railway.app']
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
