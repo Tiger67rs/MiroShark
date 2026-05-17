6bd354b1a7b98c39c372c562c2eb8bef28600cbf
Tiger67rs <tiger67rs@Yahoo.com>
2026-05-16 17:22:35 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    host: true,
+    allowedHosts: true
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
